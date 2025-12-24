dfd8899aa12e7b1ea78206e38e481620d76c3b01
tjoeun42 <tjoeunit@gmail.com>
2025-12-24 20:21:34 +0900

    2일차_5교시_mapfunction

--- dart/bin/24.inheritance.dart
+++ dart/bin/24.inheritance.dart
@@ -15,10 +15,13 @@ class Idol {
 }
 
 class BoyGroup extends Idol {
+  // 1.
   /*
   BoyGroup(String name, int membersCount)
     : super(membersCount: membersCount, name: name);
   */
+
+  // 2.
   BoyGroup({required super.membersCount, required super.name});
 }
 void main() {
@@ -26,6 +29,10 @@ void main() {
   idol1.sayName();
   idol1.sayMembersCount();
   print('-------------------------------');
+  // 1. 생성자 사용시
+  // BoyGroup bts = BoyGroup('BTS', 7);
+
+  // 2. 생성자 사용시
   BoyGroup bts = BoyGroup(name: 'BTS', membersCount: 7);
   bts.sayName();
   bts.sayMembersCount();
