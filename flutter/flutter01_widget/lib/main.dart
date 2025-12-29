92d61deb5b5398ebc7d8aafc36f90988dd0972c4
tjoeun42 <tjoeunit@gmail.com>
2025-12-29 11:51:36 +0900

    4일차_2교시_flutter widget

--- flutter/flutter01_widget/lib/main.dart
+++ flutter/flutter01_widget/lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
+/*
+* Widget
+  - Meterial widget과 Cupertino widget
+    : 위젯을 사용하려면 반드시 2개 중 하나를 사용해야 그 안에 있는 디자인을 기반으로 위젯이 만들어짐
+      (미리 만들어 놓은 위젯들을 가져다 사용함. 컴포넌트(or 라이브러리)가 들어있다 생각하면 됨)
+      위젯들이 정상 작동하려면 반드시 넣어야 됨
+    1. Meterial widget : 안드로이드용 widget
+    2. Cupertino widget : iPhone용 widget
+    > flutter에서 안드로이드 폰을 만들어도 Cupertino widget 사용 가능. 서로 교차 사용 가능
 
+    * 기본적으로 많이 사용하는 widget
+      1. Text() : 글씨 넣기 -> Text('글씨')
+      2. Image() : 이미지 넣기 -> Image.asset('이미지명')
+      3. Icon() : 아이콘 넣기 -> Icon(Icon.??)
+      4. Container : 박스 넣기
+ */
 void main() {
   runApp(const MyApp());
 }
@@ -7,116 +22,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // 첫 번째 화면 설정. 기본적으로 앱이 시작되면 이 화면이 가장 먼저 보임
+      // home: Text('글씨'),
+      // home: Icon(Icons.star),
+
+      // home: Image.asset('cat.png'),   // 크롬에서 볼 때
+      // home: Image.asset('assets/cat.png'),   // 애뮬레이터에서 볼 때, 최신버전은 크롬,애뮬 다보임
+
+      // home: Container(color: Colors.lightGreen,),
+      // width, height가 안되는 이유는 박스의 기준(어디를 기준으로 width:50, height:50을 넣어야 되는지)이 없기 때문
+      home: Container(width: 50, height: 50, color: Colors.lightGreen,),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
