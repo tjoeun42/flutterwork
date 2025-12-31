bbecc82899ad433052c9793b544af9017e884b84
tjoeun42 <tjoeunit@gmail.com>
2025-12-31 10:33:17 +0900

    6일차_1교시_inputData

--- flutter/flutter03_state/lib/main.dart
+++ flutter/flutter03_state/lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+/*
+1. 1단계
+     TextField에 글씨 넣지 않고 그냥 완료버튼만 누르면 name에
+        이름 추가하기
+     부모위젯의 name state를 변경
+     1.1 수정함수
+     1.2 함수를 보내고, 등록하고, 사용하기
 
+2. 2단계
+     input 데이터를 변수에 저장하여 보내기
+ */
 void main() {
   runApp(
       MaterialApp(
@@ -18,6 +28,22 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var name = ['이기쁨', '채규태', '이고잉'];
 
+/*
+  // 1.1 수정함수 만들기
+  addName() {
+    setState(() {
+      name.add('나생성');
+    });
+  }
+*/
+
+  // 2.1 수정함수 만들기
+  addName(inputName) {
+    setState(() {
+      name.add(inputName);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,10 +51,11 @@ class _MyAppState extends State<MyApp> {
         onPressed: () {
           print(context);
           showDialog(
-              context: context,
-              builder: (context) {
-                return CustomDialog();
-              }
+            context: context,
+            builder: (context) {
+// 1.2. 자식에게 함수 보내기.  2.2 동일
+              return CustomDialog(addName : addName);
+            }
           );
         },
         child: Text('Dialog'),
@@ -54,8 +81,13 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
+// 1.3 등록.  2.3 동일
 class CustomDialog extends StatelessWidget {
-  CustomDialog({super.key});
+  CustomDialog({super.key, this.addName});
+  final addName;
+
+  // 2.4 input데이터를 저장할 변수
+  var inputData = '';
 
   @override
   Widget build(BuildContext context) {
@@ -65,9 +97,15 @@ class CustomDialog extends StatelessWidget {
         height: 300,
         child: Column(
           children: [
-            TextField(),
-            TextButton(onPressed: (){}, child: Text('완료')),
-            TextButton(onPressed: (){}, child: Text('취소'))
+            // 2.5 받은 이름 변수에 저장
+            TextField(onChanged: (text){ inputData = text; }),
+
+            // 1.4 사용하기
+            // TextButton(onPressed: (){addName(); Navigator.pop(context);}, child: Text('완료')),
+
+            // 2.6 사용하기
+            TextButton(onPressed: (){addName(inputData); Navigator.pop(context);}, child: Text('완료')),
+            TextButton(onPressed: (){Navigator.pop(context);}, child: Text('취소'))
           ],
         ),
       ),
