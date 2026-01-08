14644932015ae7f627fec68067207c6be6935d90
tjoeun42 <tjoeunit@gmail.com>
2026-01-08 10:50:52 +0900

    11일차_1교시_회원가입

--- flutter/flutter06_springboot_signup/lib/screens/signup_page.dart
+++ flutter/flutter06_springboot_signup/lib/screens/signup_page.dart
@@ -1,4 +1,7 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+
+import '../services/db_service.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -8,6 +11,48 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final DBService dbService = DBService();
+
+  // GlobalKey<FormState> : 폼의 유효성 검사를 하는 메소드
+  // TextFormField의 validator 함수들을 실행하고, 모든 유효성 검사를 통과하면 true 반환
+  final GlobalKey<FormState> emailFormKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> passwordFormKey = GlobalKey<FormState>();
+
+  int _currentStep = 0;   // step의 번호
+  Map<String, dynamic> userInputData = {};
+
+  final Map<String, TextEditingController> controllers = {
+    'email' : TextEditingController(),
+    'password' : TextEditingController(),
+    'name' : TextEditingController(),
+    'birth' : TextEditingController(),
+    'gender' : TextEditingController()
+  };
+
+  Map<String, bool> validation = {'email' : true};
+
+  List<Step> get stepList => [
+    Step(
+      title: Text('Email'),
+      content: Form(
+        key: emailFormKey,
+        child: TextFormField(
+          controller: controllers['email'],
+          decoration: InputDecoration(hintText: '이메일을 입력하세요'),
+          keyboardType: TextInputType.emailAddress,
+          onChanged: (value) => userInputData['email'] = value,
+          validator: (value) {
+            if(value == null || value.isEmpty) {
+              return '이메일은 반드시 입력해야 합니다';
+            }
+            if(EmailValidator.validate(value)) {
+              return '유효한 이메일을 입력해 주세요';
+            }
+          },
+        )
+      )
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
--- flutter/flutter06_springboot_signup/lib/services/db_service.dart
+++ flutter/flutter06_springboot_signup/lib/services/db_service.dart
@@ -1,15 +1,39 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
+import '../models/user_model.dart';
 
-class DbService {
+class DBService {
   final String baseUrl = "http://172.16.250.4:8080";    // Springboot api 주소
 
   Future<bool> emailCheck(String email) async {
     final response = await http.get(Uri.parse("$baseUrl/flutter/email-check?email=$email"));
     if(response.statusCode == 200) {
+      // 서버에서 Map<"isAvaliable", false> 이런식을 보낼예정
       return jsonDecode(response.body)['isAvaliable'];
     }
     throw Exception("이메일 확인 실패");
   }
+
+  Future<void> registerUser(User user) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/flutter/insert"),
+      headers: {'Content-Type' : 'application/json'},
+      body: jsonEncode(user.toJson())
+    );
+    if(response.statusCode != 200) {
+      throw Exception("회원가입 실패");
+    }
+
+    Future<List<User>> getAllUsers() async {
+      final response = await http.get(Uri.parse("$baseUrl/flutter/users"));
+      if(response.statusCode == 200) {
+        List<dynamic> userList = jsonDecode(response.body);
+        return userList.map((u) => User.fromJson(u)).toList();
+      } else {
+        throw Exception("모든 사용자정보 가져오기 실패 : ${response.statusCode}");
+      }
+    }
+  }
 }
+
+
